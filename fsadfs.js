976476d3c9eec9aaf8ab26740af109e2f70e57d9
leoTutoring <leo@tutoring.co.kr>
2019-08-28 17:02:42 +0900

    update fileC

--- fsadfs.js
+++ fsadfs.js
@@ -1,1 +1,18 @@
 console.log("hello world");
+asdkgljasljkgaslkgjas
+gaskl
+asdkgljasljkgaslkgjasl;gjalksjgalsjg
+class gasjga {
+	klsgjkl
+	asglk
+	sjg
+	lasjg
+	lsagl
+	ksglk
+	static gsa
+	lgjasl
+	kgf
+	lsf
+	lsaf
+	las
+}
